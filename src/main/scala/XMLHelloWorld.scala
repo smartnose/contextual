aca0697d0e9cf43503fc654cbcb9cf5c43d5c675
Wei Liu <weil@zillowgroup.com>
2017-03-11 17:14:21 -0800

    Rome now works with bundled depdencies

--- src/main/scala/XMLHelloWorld.scala
+++ src/main/scala/XMLHelloWorld.scala
@@ -1,6 +1,28 @@
+import java.net.URL
+
+import com.rometools.rome.io.{SyndFeedInput, XmlReader}
+
 object XMLHelloWorld {
   def main(args: Array[String]): Unit = {
     val xml = <hello>world</hello>
     println(xml)
-  }
+
+        var ok = false;
+
+      try {
+        val feedUrl = new URL("http://feeds.reuters.com/reuters/topNews");
+
+        val input = new SyndFeedInput();
+        val feed = input.build(new XmlReader(feedUrl));
+
+        System.out.println(feed);
+
+        ok = true;
+      }
+      catch  {
+        case ex:Exception =>
+          ex.printStackTrace();
+          println("ERROR: "+ex.getMessage());
+      }
+    }
 }
