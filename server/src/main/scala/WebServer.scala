7790e32a9ff918aec78759ecaadd4ff2a610f58c
Wei Liu <smartnose@gmail.com>
2017-04-14 13:56:32 -0700

    simple http response

--- server/src/main/scala/WebServer.scala
+++ server/src/main/scala/WebServer.scala
@@ -6,6 +6,12 @@ import akka.http.scaladsl.server.Directives._
 
 import scala.io.StdIn
 
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+
+case class Item(name: String, id: Long)
+
 /**
   * @author weil
   */
@@ -17,12 +23,15 @@ object WebServer {
         // needed for the future flatMap/onComplete in the end
         implicit val executionContext = system.dispatcher
 
-        val route =
-            path("") {
-                get {
-                    complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
-                }
-            }
+
+      // formats for unmarshalling and marshalling
+      implicit val itemFormat = jsonFormat2(Item)
+      
+        val route = get {
+          pathPrefix("dict" /  Segment) {
+            word => complete(new Item(word, 0L))
+          }
+        }
 
         val bindingFuture = Http().bindAndHandle(route, "127.0.0.1", 8020)
 
